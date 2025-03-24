89dffb40d2f27d8ed8d8532283cb538e1d14e835
Marcos Albuquerque <marcosg.as2232@gmail.com>
2025-03-23 23:42:19 -0300

    criacao do env

--- script.mjs
+++ script.mjs
@@ -2,14 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase
 import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuSKTNAKTKUnbVZwiaAp_0oAZfxFajFio",
-  authDomain: "loginbsfb.firebaseapp.com",
-  projectId: "loginbsfb",
-  storageBucket: "loginbsfb.firebasestorage.app",
-  messagingSenderId: "591007489590",
-  appId: "1:591007489590:web:1d718c2659d8f6771f4297"
-};
-
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+  };
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
